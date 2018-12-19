912c134f5bd77d141264803ffb132714158e1774
mGiorgiutti <mathieu.giorgiutti@gfi.fr>
2018-12-19 17:42:21 +0100

    using ngx-bootstrap for button toggle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { NgxMultiselectComponent } from './components/ngx-multiselect/ngx-multiselect.component';
+import { AppComponent } from "./app.component";
+import { NgxMultiselectComponent } from "./components/ngx-multiselect/ngx-multiselect.component";
+import { FormsModule } from "@angular/forms";
+import { BsDropdownModule } from "ngx-bootstrap/dropdown";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NgxMultiselectComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, NgxMultiselectComponent],
+  imports: [BrowserModule, FormsModule, BsDropdownModule.forRoot()],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/ngx-multiselect/ngx-multiselect.component.ts
+++ src/app/components/ngx-multiselect/ngx-multiselect.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-ngx-multiselect',
-  templateUrl: './ngx-multiselect.component.html',
-  styleUrls: ['./ngx-multiselect.component.scss']
+  selector: "app-ngx-multiselect",
+  templateUrl: "./ngx-multiselect.component.html",
+  styleUrls: ["./ngx-multiselect.component.scss"]
 })
 export class NgxMultiselectComponent implements OnInit {
-
-  constructor() { }
+  selectedItems: string;
+  items: any[] = [];
+  constructor() {}
 
   ngOnInit() {
+    this.selectedItems = "";
+    this.items.push(
+      {
+        id: 1,
+        name: "United-States"
+      },
+      {
+        id: 2,
+        name: "France"
+      },
+      {
+        id: 3,
+        name: "Japan"
+      },
+      {
+        id: 4,
+        name: "Australia"
+      },
+      {
+        id: 5,
+        name: "Great Britain"
+      }
+    );
   }
-
 }

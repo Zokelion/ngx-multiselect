aac2f6f04ae51f436ba333932f23a2cbf0759ed0
mGiorgiutti <mathieu.giorgiutti@gfi.fr>
2018-12-19 16:34:45 +0100

    add component ngx-multiselect

---
+++ src/app/components/ngx-multiselect/ngx-multiselect.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ngx-multiselect',
+  templateUrl: './ngx-multiselect.component.html',
+  styleUrls: ['./ngx-multiselect.component.scss']
+})
+export class NgxMultiselectComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

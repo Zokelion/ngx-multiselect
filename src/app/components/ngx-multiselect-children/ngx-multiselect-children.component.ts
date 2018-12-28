01bb03b21b794940bd751281bb6c8dfd171e113a
mGiorgiutti <mathieu.giorgiutti@gfi.fr>
2018-12-28 14:28:19 +0100

    change algo and select branch when all leaf are selected

--- src/app/components/ngx-multiselect-children/ngx-multiselect-children.component.ts
+++ src/app/components/ngx-multiselect-children/ngx-multiselect-children.component.ts
@@ -1,23 +1,172 @@
-import { Component, OnInit, Input } from "@angular/core";
+import {
+    Component,
+    OnInit,
+    Input,
+    Output,
+    EventEmitter,
+    SimpleChange,
+    OnChanges,
+    SimpleChanges
+} from '@angular/core';
+import { ItemSelectedEvent } from '../../models/item-selected-event.model';
 
 @Component({
-  selector: "app-ngx-multiselect-children",
-  templateUrl: "./ngx-multiselect-children.component.html",
-  styleUrls: ["./ngx-multiselect-children.component.scss"]
+    selector: 'app-ngx-multiselect-children',
+    templateUrl: './ngx-multiselect-children.component.html',
+    styleUrls: ['./ngx-multiselect-children.component.scss']
 })
-export class NgxMultiselectChildrenComponent implements OnInit {
-  @Input()
-  selectAll: boolean;
-  @Input()
-  unSelectAll: boolean;
-  @Input()
-  items: any[];
-  @Input()
-  filter: any;
-  @Input()
-  isFirstLevel: boolean;
+export class NgxMultiselectChildrenComponent implements OnInit, OnChanges {
+    @Input()
+    filter: any;
+    @Input()
+    isFirstLevel: boolean;
+    @Input()
+    includeContainer: boolean;
+    @Input()
+    parentItem: any;
 
-  constructor() {}
+    selectedItems: any[] = [];
 
-  ngOnInit() {}
+    @Output()
+    itemSelected: EventEmitter<ItemSelectedEvent> = new EventEmitter<ItemSelectedEvent>();
+
+    constructor() {}
+
+    ngOnInit() {
+        this.setSelectedItems();
+    }
+
+    ngOnChanges(changes: SimpleChanges): void {
+        console.log(this._countChildren(), 'children found for', this.parentItem.name);
+    }
+
+    public selectItem(): void {
+        if (this.parentItem.children.length === 0) {
+            // Here we have no children and no selected item => We're handling a click on an unchecked leaf
+            if (this.selectedItems.length === 0) {
+                this.parentItem.isSelected = true;
+                this.selectedItems.push(this.parentItem);
+                this.itemSelected.emit({
+                    item: this.parentItem,
+                    selectedItems: this.selectedItems
+                });
+            } else {
+                // Here we have no children and a selected item => We're handling a click on an checked leaf
+                this.parentItem.isSelected = false;
+                this.selectedItems.splice(this.selectedItems.indexOf(this.parentItem), 1);
+                this.itemSelected.emit({
+                    item: this.parentItem,
+                    selectedItems: this.selectedItems
+                });
+            }
+        } else {
+            if (this.parentItem.isSelected) {
+                // Here we have childrens and a selected parent, we check if parent must be include in the list and add all childrens of the branch
+                if (this.includeContainer) {
+                    this.parentItem.isSelected = true;
+                    this.selectedItems.push(this.parentItem);
+                    this.itemSelected.emit({
+                        item: this.parentItem,
+                        selectedItems: this.selectedItems
+                    });
+                }
+                this.selectAllChildren();
+            } else {
+                // Here we have childrens and an unselected parent, we check if parent must be include in the list and remove all childrens of the branch
+                if (this.includeContainer) {
+                    this.parentItem.isSelected = false;
+                    this.selectedItems.splice(this.selectedItems.indexOf(this.parentItem), 1);
+                    this.itemSelected.emit({
+                        item: this.parentItem,
+                        selectedItems: this.selectedItems
+                    });
+                }
+                this.unSelectAllChildren();
+            }
+        }
+    }
+
+    public selectAllChildren(items?: any[]): void {
+        if (!items) {
+            items = this.parentItem.children;
+        }
+        items.forEach(item => {
+            item.isSelected = true;
+            if (item.children.length > 0) {
+                this.selectAllChildren(item.children);
+                if (this.includeContainer) {
+                    this.selectedItems.push(item);
+                    this.itemSelected.emit({ item, selectedItems: this.selectedItems });
+                }
+            } else {
+                this.selectedItems.push(item);
+                this.itemSelected.emit({ item, selectedItems: this.selectedItems });
+            }
+        });
+    }
+
+    public unSelectAllChildren(items?: any[]): void {
+        if (!items) {
+            items = this.parentItem.children;
+        }
+        items.forEach(item => {
+            item.isSelected = false;
+            if (item.children.length > 0) {
+                this.unSelectAllChildren(item.children);
+                if (this.includeContainer) {
+                    this.selectedItems.splice(this.selectedItems.indexOf(item), 1);
+                    this.itemSelected.emit({ item, selectedItems: this.selectedItems });
+                }
+            } else {
+                this.selectedItems.splice(this.selectedItems.indexOf(item), 1);
+                this.itemSelected.emit({ item, selectedItems: this.selectedItems });
+            }
+        });
+    }
+
+    public childSelected(eventItem: ItemSelectedEvent): void {
+        if (this.selectedItems.indexOf(eventItem.item) !== -1 && !eventItem.item.isSelected) {
+            this.selectedItems.splice(this.selectedItems.indexOf(eventItem.item), 1);
+        } else if (eventItem.item.isSelected && this.selectedItems.indexOf(eventItem.item) === -1) {
+            this.selectedItems.push(eventItem.item);
+        }
+
+        if (this.selectedItems.length === this._countChildren()) {
+            this.parentItem.isSelected = true;
+            if (this.includeContainer) {
+                this.selectedItems.push(this.parentItem);
+            }
+        } else {
+            this.parentItem.isSelected = false;
+            if (this.includeContainer && this.selectedItems.indexOf(this.parentItem) !== -1) {
+                this.selectedItems.splice(this.selectedItems.indexOf(this.parentItem), 1);
+            }
+        }
+        this.itemSelected.emit({ item: eventItem.item, selectedItems: this.selectedItems });
+    }
+
+    // set selectedItems list on the firstLoad of the page
+    public setSelectedItems(): void {
+        this.parentItem.children.forEach(item => {
+            // this.childSelected({ item });
+        });
+    }
+    private _countChildren(item?: any): number {
+        let acc = 0;
+        if (!item) {
+            item = this.parentItem;
+        }
+        if (item.children.length) {
+            item.children.forEach((child: any) => {
+                acc += this._countChildren(child);
+            });
+            if (this.includeContainer) {
+                acc++;
+            }
+        } else {
+            acc++;
+        }
+
+        return acc;
+    }
 }
--- src/app/components/ngx-multiselect/ngx-multiselect.component.ts
+++ src/app/components/ngx-multiselect/ngx-multiselect.component.ts
@@ -1,157 +1,153 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
+import { ItemSelectedEvent } from '../../models/item-selected-event.model';
 
 @Component({
-  selector: "app-ngx-multiselect",
-  templateUrl: "./ngx-multiselect.component.html",
-  styleUrls: ["./ngx-multiselect.component.scss"]
+    selector: 'app-ngx-multiselect',
+    templateUrl: './ngx-multiselect.component.html',
+    styleUrls: ['./ngx-multiselect.component.scss']
 })
 export class NgxMultiselectComponent implements OnInit {
-  selectedItems: any[] = [];
-  items: any[] = [];
-  filter: any = { id: null, name: "", isSelected: false };
-  displaySelectedValue: string;
-  selectAll: Boolean;
-  unSelectAll: Boolean;
+    selectedItems: any[] = [];
+    items: any[] = [];
+    filter: any = { id: null, name: '', isSelected: false };
+    displaySelectedValue: string;
+    buttonLabel: string;
+    includeContainer: boolean;
 
-  constructor() {}
+    constructor() {}
 
-  ngOnInit() {
-    this.selectedItems = [];
-    this.items.push({
-      id: 1,
-      name: "World",
-      isSelected: false,
-      children: [
-        {
-          id: 2,
-          name: "America",
-          isSelected: false,
-          children: [
-            {
-              id: 3,
-              name: "Canada",
-              isSelected: false,
-              children: []
-            },
-            {
-              id: 4,
-              name: "United-States",
-              isSelected: false,
-              children: [
+    ngOnInit() {
+        this.selectedItems = [];
+        this.items.push({
+            id: 1,
+            name: 'World',
+            isSelected: false,
+            children: [
                 {
-                  id: 14,
-                  name: "Arizona",
-                  isSelected: false,
-                  children: []
+                    id: 2,
+                    name: 'America',
+                    isSelected: false,
+                    children: [
+                        {
+                            id: 3,
+                            name: 'Canada',
+                            isSelected: false,
+                            children: []
+                        },
+                        {
+                            id: 4,
+                            name: 'United-States',
+                            isSelected: false,
+                            children: [
+                                {
+                                    id: 14,
+                                    name: 'Arizona',
+                                    isSelected: false,
+                                    children: []
+                                },
+                                {
+                                    id: 15,
+                                    name: 'Washington',
+                                    isSelected: false,
+                                    children: []
+                                }
+                            ]
+                        },
+                        {
+                            id: 5,
+                            name: 'Mexico',
+                            isSelected: false,
+                            children: []
+                        }
+                    ]
                 },
                 {
-                  id: 15,
-                  name: "Washington",
-                  isSelected: false,
-                  children: []
+                    id: 6,
+                    name: 'Europe',
+                    isSelected: false,
+                    children: [
+                        {
+                            id: 7,
+                            name: 'France',
+                            isSelected: true,
+                            children: []
+                        },
+                        {
+                            id: 8,
+                            name: 'Deutschland',
+                            isSelected: true,
+                            children: []
+                        }
+                    ]
+                },
+                {
+                    id: 9,
+                    name: 'Asia',
+                    isSelected: false,
+                    children: [
+                        {
+                            id: 10,
+                            name: 'China',
+                            isSelected: true,
+                            children: []
+                        },
+                        {
+                            id: 11,
+                            name: 'Japan',
+                            isSelected: true,
+                            children: []
+                        }
+                    ]
+                },
+                {
+                    id: 12,
+                    name: 'Oceania',
+                    isSelected: true,
+                    children: [
+                        {
+                            id: 13,
+                            name: 'Australia',
+                            isSelected: false,
+                            children: []
+                        }
+                    ]
                 }
-              ]
-            },
-            {
-              id: 5,
-              name: "Mexico",
-              isSelected: false,
-              children: []
-            }
-          ]
-        },
-        {
-          id: 6,
-          name: "Europe",
-          isSelected: false,
-          children: [
-            {
-              id: 7,
-              name: "France",
-              isSelected: true,
-              children: []
-            },
-            {
-              id: 8,
-              name: "Deutschland",
-              isSelected: true,
-              children: []
-            }
-          ]
-        },
-        {
-          id: 9,
-          name: "Asia",
-          isSelected: false,
-          children: [
-            {
-              id: 10,
-              name: "China",
-              isSelected: true,
-              children: []
-            },
-            {
-              id: 11,
-              name: "Japan",
-              isSelected: true,
-              children: []
-            }
-          ]
-        },
-        {
-          id: 12,
-          name: "Oceania",
-          isSelected: true,
-          children: [
-            {
-              id: 13,
-              name: "Australia",
-              isSelected: false,
-              children: []
-            }
-          ]
-        }
-      ]
-    });
-    this.setSelectedItems();
-  }
-
-  public selectItem(item: any): void {
-    if (item.isSelected) {
-      item.isSelected = false;
-      this.selectedItems.splice(this.selectedItems.indexOf(item), 1);
-    } else {
-      item.isSelected = true;
-      this.selectedItems.push(item);
+            ]
+        });
+        this.setLabel();
     }
-  }
 
-  public selectAllItems(): void {
-    this.selectAll = true;
-    this.unSelectAll = false;
+    public selectAllItems(items: any[]): void {
+        items.forEach(item => {
+            item.isSelected = true;
+            if (item.children.length > 0) {
+                this.selectAllItems(item.children);
+            }
+            this.selectedItems.push(item);
+        });
+    }
 
-    this.items.forEach(item => {
-      item.isSelected = true;
-      this.selectedItems.push(item);
-    });
-  }
+    public unSelectAllItems(items: any[]): void {
+        items.forEach(item => {
+            item.isSelected = false;
+            if (item.children.length > 0) {
+                this.unSelectAllItems(item.children);
+            }
+        });
+        this.selectedItems = [];
+    }
 
-  public unSelectAllItems(): void {
-    this.unSelectAll = true;
-    this.selectAll = false;
+    public childSelected(eventItem: ItemSelectedEvent): void {
+        this.selectedItems = eventItem.selectedItems;
+        console.log('selected : ', this.selectedItems);
+        this.setLabel();
+    }
 
-    this.items.forEach(item => {
-      item.isSelected = false;
-    });
-    this.selectedItems = [];
-  }
-
-  //set selectedItems list on the firstLoad of the page
-  public setSelectedItems(): void {
-    this.items.forEach(item => {
-      if (item.isSelected) {
-        this.selectedItems.push(item);
-      }
-    });
-  }
+    public setLabel(): void {
+        if (this.selectedItems.length === 0) {
+            this.buttonLabel = 'No Items Selected';
+        } else {
+            this.buttonLabel = this.selectedItems[0].name + ', (' + this.selectedItems.length + ')';
+        }
+    }
 }
